909c182e59d637cc628ac1ea9f097ce0c997c325
Marc Combarel <marc.combarel@edhec.com>
2020-02-11 12:24:51 +0100

    added routes for Task CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # CRUD
+
+  # Create
+  get 'tasks', to: 'tasks#new', as: :task
+  post 'tasks', to: 'tasks#create'
+
+  # Read (All // One specific task)
+  get 'tasks', to: 'tasks#index', as: :task
+  get 'tasks/:id', to: 'tasks#show'
+
+  # Update (Get the page of the element we want to update // We update)
+  get 'tasks/:id/edit', to: 'tasks#edit'
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # Delete
+  delete 'tasks/:id', to: 'restaurant#destroy'
 end
