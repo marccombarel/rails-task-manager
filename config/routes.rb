c148ff212f6ed94fcfca2b58885b69d1b43d57a8
Marc Combarel <marc.combarel@edhec.com>
2020-02-11 12:43:25 +0100

    added routes to TasksController

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # CRUD
 
-  # Create
-  get 'tasks', to: 'tasks#new', as: :task
+  # # Create
+  get 'tasks/new', to: 'tasks#new'
   post 'tasks', to: 'tasks#create'
 
   # Read (All // One specific task)
-  get 'tasks', to: 'tasks#index', as: :task
+  get 'tasks', to: 'tasks#index'
   get 'tasks/:id', to: 'tasks#show'
 
   # Update (Get the page of the element we want to update // We update)
